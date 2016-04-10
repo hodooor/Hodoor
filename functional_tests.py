e9b20c257d83f1f67c41dc45927e39132706b218
Ondrej Vicar <ondrej.vicar@eledus.cz>
2016-04-10 20:25:44 +0200

    Reworked functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+SERVER_URL = "http://localhost:8000"
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_ticker_in_title(self):
+		self.browser.get(SERVER_URL)
+		self.assertIn('ticker', self.browser.title)
+
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main()
