0bd4a9123b043ffe00bd9a46e6c0cab67bea1233
Ondrej Vicar <ondrej.vicar@eledus.cz>
2016-04-13 23:37:43 +0200

    assertion error

--- test_rest_population.py
+++ test_rest_population.py
@@ -22,35 +22,37 @@ class ApiPostSwipesTest(unittest.TestCase):
 	#generate users
 	user = 1 #ondrej.vicar
 	
-	#generte datetimes
-	datetimes = []
-	t = datetime.now()
-	
 	#generate swipetypes
 	swipe_types = ["IN","OBR","FBR","OUT","IN","OUT","IN","OBR",
 		"FBR","OBR","FBR","OUT"]
 	hours_increment = 1
 
+		#generte datetimes
+	datetimes = []
+	t = datetime.now()
+
 	for swipe in swipe_types:
 		datetimes.append((t + timedelta(hours = hours_increment)).isoformat())
 		hours_increment += 1 
+	
+	data = []
+
+	for swipe_type,datetime, in zip(swipe_types,datetimes):
+		data.append({"user":user,"swipe_type":swipe_type,"datetime":datetime})	
+
 	
 	def post_some_swipes(self):
 		"""
 		Populates database with some swipes thru REST API
 		"""
-		
-
+	
 		POST_URL = "http://127.0.0.1:8000/swipes/"
 		HEADERS = {'Content-Type': 'application/json'}
 
 		#need to crete list of dictionaries:
-		data = []
+		
 
-		for swipe_type,datetime, in zip(self.swipe_types,self.datetimes):
-			data.append({"user":self.user,"swipe_type":swipe_type,"datetime":datetime})
-
-		for swipe in data:
+		for swipe in self.data:
 			r = requests.post(POST_URL, json.dumps(swipe), headers = HEADERS)
 			if(r.status_code == 201): #CREATED 201
 				print("POSTED:	" + str(swipe))
@@ -58,7 +60,10 @@ class ApiPostSwipesTest(unittest.TestCase):
 	def get_posted_data_from_database(self):
 		s = Swipe.objects.all()
 
-		for swipe in s:
+		for swipe,data_constant in zip(s,self.data):
+			self.assertEqual(swipe.user.id,data_constant["user"])
+			self.assertEqual(swipe.swipe_type,data_constant["swipe_type"])
+			self.assertEqual(swipe.datetime.isoformat(),data_constant["datetime"])
 			print(swipe)
 		s.delete()
 
