4d84af36a90ed2f99fd675f5ed6637d87cefc015
Ondrej Vicar <ondrej.vicar@eledus.cz>
2016-05-28 22:58:27 +0200

    Testing login and logout users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,26 +6,26 @@ from attendance.serializers import UserSerializer, SwipeSerializer
 from django.contrib.auth.models import User
 from django.conf import settings
 
+def populate_database(what_pop):
+	what_pop.USERS = USERS
+	what_pop.SWIPES = SWIPES
+	what_pop.SWIPE_TYPES = SWIPE_TYPES
+	what_pop.TEST_PASSWORD = "user1234"
+
+	dict_to_database(UserSerializer,what_pop.USERS)
+	dict_to_database(SwipeSerializer,what_pop.SWIPES)
+
+	users = User.objects.all()
+	
+	for user in users:
+		user.set_password(what_pop.TEST_PASSWORD)
+		user.save()
+
 class NewVisitorTest(LiveServerTestCase):
+		#Prepopulate database with test data
+
 
 	def setUp(self):
-		#Prepopulate database with test data
-		if settings.DEBUG == False:
-			settings.DEBUG = True
-
-		self.USERS = USERS
-		self.SWIPES = SWIPES
-		self.SWIPE_TYPES = SWIPE_TYPES
-		self.TEST_PASSWORD = "user1234"
-
-		dict_to_database(UserSerializer,self.USERS)
-		dict_to_database(SwipeSerializer,self.SWIPES)
-
-		users = User.objects.all()
-		
-		for user in users:
-			user.set_password(self.TEST_PASSWORD)
-			user.save()
 
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -44,12 +44,20 @@ class NewVisitorTest(LiveServerTestCase):
 		header_text = self.browser.find_element_by_tag_name('p').text
 		self.assertIn("login", header_text)
 
-		username = self.browser.find_element_by_id("id_username")
-		password = self.browser.find_element_by_id("id_password")
 
-		username.send_keys(self.USERS[0]["username"])
-		password.send_keys(self.TEST_PASSWORD)
-		self.browser.find_element_by_css_selector("input[value='login']").click()
+	def test_login_and_logut_users(self):
+		populate_database(self)
 
-		sessions_header = self.browser.find_element_by_tag_name("h1").text
-		self.assertIn("Sessions", sessions_header)
+		for user in self.USERS:
+			self.browser.get(self.live_server_url)
+			username = self.browser.find_element_by_id("id_username")
+			password = self.browser.find_element_by_id("id_password")
+			
+			username.send_keys(user["username"])
+			password.send_keys(self.TEST_PASSWORD)
+			self.browser.find_element_by_css_selector("input[value='login']").click()
+
+			sessions_header = self.browser.find_element_by_tag_name("h1").text
+			self.assertIn("Sessions", sessions_header)
+
+			self.browser.get(self.live_server_url+ "/logout/")
