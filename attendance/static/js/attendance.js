830be4a8fc32d919e01c6268065d188c51415c0d
patrik.predny <patrik.predny@eledus.cz>
2016-06-21 10:53:58 +0200

    Swipes template edited: added function for displaying all of swipes, in JS (jQuery) - good job Ondrasku

--- attendance/static/js/attendance.js
+++ attendance/static/js/attendance.js
@@ -1,4 +1,17 @@
 //setInterval(function(){ console.log("Ahoj Patričku :)"); }, 1000);
 $(document).ready(function(){
+	//Tooltip info in SWIPES template
     $('[data-toggle="tooltip"]').tooltip(); //enables tooltip
+
+    //Hidding and showing All of the swipes in SWIPES template
+    var rows = $('table.table tbody tr');
+	$('#showAll').click(function() {
+	    rows.filter('.hiddenRow').show();
+    	$('#showAll').addClass("hide");	
+	});
+	var MaxNumOfRow = 12;
+	rows.filter(function( index ) { return index < MaxNumOfRow; }).removeClass("hiddenRow"); //if rowIndex is lower than VALUE then remoce hidding
+
 });
+
+
