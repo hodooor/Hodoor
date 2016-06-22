8192aa02ad17239a7a97c1822ce34bd1dcdee0e9
Ondrej Vicar <ondrej.vicar@eledus.cz>
2016-06-22 21:44:05 +0200

    Added cookie secure to template

--- ticker/settings.py
+++ ticker/settings.py
@@ -16,7 +16,6 @@ from .settings_secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Application definition
 
 INSTALLED_APPS = [
