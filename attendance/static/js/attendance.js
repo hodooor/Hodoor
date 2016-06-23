24fa7c9449d6f69e3b3af22ca367c1201a3949ef
patrik.predny <patrik.predny@eledus.cz>
2016-06-23 10:44:53 +0200

    Swipe template page, attendance.js - button for hidding/showing all records updated correctly

--- attendance/static/js/attendance.js
+++ attendance/static/js/attendance.js
@@ -1,17 +1,54 @@
-//setInterval(function(){ console.log("Ahoj Patričku :)"); }, 1000);
-$(document).ready(function(){
-	//Tooltip info in SWIPES template
+$(document).ready(function(){ //Calling jQuery when document.ready
+	//===================================Tooltip=========================================
   $('[data-toggle="tooltip"]').tooltip(); //enables tooltip
 
-  //Hidding and showing All of the swipes in SWIPES template
-  var rows = $('table.table tbody tr');
-	$('#showAll').click(function() {
+  //===================================Show/Hide Swipes================================
+  var SwipesBtn = $('#showAll'); //get identifier for clickable button
+  var MaxNumOfRow = 12; //how many rows should be vissible
+  var btnText1 = "View All Swipes";
+  var btnText2 = "Hide until last " + MaxNumOfRow;
+  var btnClass1 = "btn-primary";
+  var btnClass2 = "btn-danger";
+  var btnTitle1 = "Click to show all of swipes";
+  var btnTitle2 = "Click to hide all records until last " + MaxNumOfRow;
+  var rows = $('table.table tbody tr'); //get row indexes
+  var rowsCount = rows.length; //get max count of table rows
+  var btnState = 0; //bool, btnState if 0 then button is activated
+
+  //calling functions after button event
+  $(SwipesBtn).click(function() {
+  	if(!btnState) {
+			$(SwipesBtn).html(btnText2);
+			$(SwipesBtn).removeClass(btnClass1);
+			$(SwipesBtn).addClass(btnClass2);
+			$(SwipesBtn).attr('data-original-title', btnTitle2)
+			HideRows(rows);	
+			btnState = !btnState;
+  	} else {
+			$(SwipesBtn).html(btnText1);
+			$(SwipesBtn).removeClass(btnClass2);
+			$(SwipesBtn).addClass(btnClass1);
+			$(SwipesBtn).attr('data-original-title', btnTitle1)
+			UnhideRows(rows);	
+  		btnState = !btnState;
+  	}
+  });
+
+  //Hidding/showing all of the swipes in SWIPES template
+  function HideRows(rows) {
 	    rows.filter('.hiddenRow').show();
-    	$('#showAll').addClass("hide");	
-	});
-	var MaxNumOfRow = 12;
-	rows.filter(function( index ) { return index < MaxNumOfRow; }).removeClass("hiddenRow"); //if rowIndex is lower than VALUE then remoce hidding
+  }
+  function UnhideRows(rows) {
+	    rows.filter('.hiddenRow').hide();
+  }
+
+	//=====================================Hidding last rows filter======================
+	//hidding last rows, if rowIndex is lower than VALUE then remove hidding
+	rows.filter(function( index ) { return index > rowsCount-MaxNumOfRow; }).removeClass("hiddenRow"); 
+	
+	//=====================================Debugging=====================================
+	//console.log(rowsCount);
+	//console.log(rows);
 
 });
-
 
