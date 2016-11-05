92bb4ad7be8ae13b7ab321a959b0a5393eb8cef7
Ondrej Vicar <ondrej.vicar@eledus.cz>
2016-11-05 10:38:45 +0100

    PEP8 admin.py

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from attendance.models import Session, Swipe, Key, Project, ProjectSeparation
-from django.forms import ModelForm
+
 
 class SwipeInline(admin.TabularInline):
     model = Swipe
 
+
 class ProjectSeparationInline(admin.TabularInline):
     model = ProjectSeparation
+
 
 class SessionAdmin(admin.ModelAdmin):
     inlines = [
             SwipeInline,
             ProjectSeparationInline,
             ]
-
-
 
 admin.site.register(Session, SessionAdmin)
 admin.site.register(Swipe)
