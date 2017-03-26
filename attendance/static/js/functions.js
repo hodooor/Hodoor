18c261dc3267e212cf07c685e6cbfbf7a8edde3c
patrik.predny <patrik.predny@eledus.cz>
2017-03-26 12:45:00 +0200

    Issue #41 closed, algoritm is ok.

--- attendance/static/js/functions.js
+++ attendance/static/js/functions.js
@@ -1,3 +1,10 @@
+/*------------------------------------------
+Author: Patrik Predny,
+Title: My own JS functions, used for Hodoor
+Year: 2017,
+License: MIT,
+------------------------------------------*/
+
 //CONFIGURATION VARIABLES
 var debug = 0; //set true or false, true enables debugging mode
 
@@ -21,45 +28,17 @@ function getRandomColor() {
 
 $(document).ready(function(){ 
     var temp;
-    //temp = $('.ext-link').html();
-    //console.log(temp);
-    //$('.ext-link').html(temp + ' <i class="fa fa-external-link" aria-hidden="true"></i>');
-    $('.ext-link').append(' <i class="fa fa-external-link" aria-hidden="true"></i>');
-    /*This functions makes color change on form icons*/
-    /*$(".focused").focus(function(){
-        var idcon;
-        idcon = "#"+(this).id+"-icon";
-        debugLog(idcon);
-        $(idcon).removeClass("icon");
-        $(idcon).addClass("icon-active");
-    });
-    $(".focused").focusout(function(){
-        var idcon;
-        idcon = "#"+(this).id+"-icon";
-        $(idcon).removeClass("icon-active");
-        $(idcon).addClass("icon");
-    });*/
-    
-    /*Another functions*/
-    //var style = $('#mystyle[rel=stylesheet]').attr('href');
-    //debugLog(style);   
-
+    $('.ext-link').append(' <i class="fa fa-external-link" aria-hidden="true"></i>'); 
     /* __DROPDOWN MENUS__*/
       // Add slideDown animation to Bootstrap dropdown when expanding.
       $('#myDropdown').on('show.bs.dropdown', function() {
         $(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
       }); 
-      /*$('#myDropdown').on('mouseover', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown('fast');
-      });*/
-
       // Add slideUp animation to Bootstrap dropdown when collapsing.
       $('#myDropdown').on('hide.bs.dropdown', function() {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
       });
-      /*$('#myDropdown').on('mouseout', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp('fast');
-      });*/
+
     /* //__DROPDOWN MENUS__*/
 
     /* __SCROLL TO TOP FUNCTIONS__*/
@@ -108,13 +87,11 @@ $(document).ready(function(){
       {
           $(this).parents('.panel').find('.panel-body').slideUp('fast');
           $(this).addClass('panel-collapsed');
-          //$(this).find('.panel-arrow').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
       } 
       else 
       {
           $(this).parents('.panel').find('.panel-body').slideDown('fast');
           $(this).removeClass('panel-collapsed');
-          //$(this).find('.panel-arrow').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
       }
     })
     $('.panel-collapsed').parents('.panel').find('.panel-body').hide();
@@ -122,81 +99,6 @@ $(document).ready(function(){
 
     //Enable same height on panels
     $('.equal-height-panels .panel').matchHeight();
-
-    /* __MAIN USERNAME COLOR MIXER__*/
-    var username = $('#main-username').text();
-        debugLog("users name is: "+username+" .");
-    var usercount, firstLetter;
-    usercount = username.length;
-        debugLog(usercount);
-        debugLog(username[0]);
-
-    var color, colorSegment1, colorSegment2;
-    function ColorMixer(usernametext) {  
-    colorSegment1 = '';
-    //colorSegment2 = '';
-      for (var i = 0; i < 6; i++ ) {
-        var help = usernametext.charCodeAt(i);
-        colorSegment1 += Math.round((help%10));
-      };
-      /*for (var i = 3; i > 0; i-- ) {
-        var help = usernametext.charCodeAt(i);
-        colorSegment2 += Math.round((help%10));
-      };*/
-      //color = "#" + colorSegment1 + colorSegment2;
-      color = "#" + colorSegment1;
-      return color;
-    }
-    
-    debugLog(ColorMixer(username));
-
-    $('#main-usericon').css("background", ColorMixer(username));
-        debugLog(color);
-    firstLetter = username[0];
-    $('#main-usericon').html(firstLetter);
-    /* //__MAIN USERNAME COLOR MIXER__*/
-
-    /* __COLOR MIXER 2 EDITION__ */
-    /*var Uls = [];
-    var UlsCount = 0;
-    var triplen = [];
-    $('body').find('.automaticColorMixer').each(function(i) {
-      Uls[i] = $(this).attr('id');
-      UlsCount++;
-    });
-
-    for(var i=0; i<UlsCount; i++) {
-
-      Uls[i] = "#"+Uls[i];
-        debugLog(Uls[i]);
-      triplen[i] = $(Uls[i]).children('li').length;
-        debugLog("len is:"+triplen[i]);
-
-      var userNames = [];
-      var userColors =[];
-      var userInitials = [];
-
-      $(Uls[i]).children('li').each(function(i) {
-        $(this).addClass('prefix_'+i); //add prefix with num to list items of ul
-        userNames.push($(this).text()); //save inner text as userName var
-      });
-      
-      for(var n=0; n<triplen[i]; n++) {
-        userInitials[n] = userNames[n][0]; //double array
-        userColors[n] = ColorMixer(userNames[n]); //generate colors
-        $(Uls[i]).children('.prefix_'+n).prepend("<span class='usericon small text-center'></span><span class='username'>");
-        $(Uls[i]).children('.prefix_'+n).children('.usericon').css("background-color", userColors[n]);
-        $(Uls[i]).children('.prefix_'+n).children('.usericon').text(userInitials[n]);
-        $(Uls[i]).children('.prefix_'+n).append("</span>");
-      }
-
-      userNames = [];
-      userColors =[];
-      userInitials = [];
-    
-    }
-    */
-    /* //__COLOR MIXER 2 EDITION__ */
 
     /* __Navigable links from h2 titles generator__*/
     //navigable-bar
@@ -217,27 +119,5 @@ $(document).ready(function(){
         }, 'fast');
     });
 
-    /* __DATA TABLES ENABLE__ */
-    // $('#users-with-sessions').DataTable();
-    // $('#users-without-sessions').DataTable();
-
-
-
 //end of document.ready()
 });
-
-
-
-
-
-
-/*Function to change stylesheet*/
-//set another style: onclick="changeStyle('css/xxx.css');"
-var sheetName;
-function changeStyle(sheetName) {
-    $('#mystyle[rel=stylesheet]').attr('href', sheetName);
-    debugLog($('#mystyle[rel=stylesheet]').attr('href'));
-
-    return 0;
-}
-
--- attendance/static/js/tooltips.js
+++ attendance/static/js/tooltips.js
@@ -1,10 +1,20 @@
 var tooltipData = [
-	"This Month Quota - show's you a quota for this month. Which menas hours you have to spare as worktime.",
-	"This Month Work Time - show's you a time that you allready spared for work in this month. Work hours is time which you spared with working. Not-work time includes time spared for breaks.",
-	"this assign time",
-	"terminal",
-	"last mnth",
-	"void",
+	"This Month Quota - showing you a quota for this month. Which means hours you have to spare as worktime.",
+	"This Month Work Time - showing you a time that you allready spared for work in this month. Work hours is time which you spared with working. Not-work time includes time spared for breaks.",
+	"This Month Assigned Time - showing you a time you assigned to projects. All the time you spend with work should be assigned to projects. You can do this in sessions page.",
+	"Today info panel - Each session has own ID, if you have any problem you can tell this ID to administrator. Current work hours are worked hours in this day you already have worked for. In panel's footer is shown last of your swipe.",
+	"Terminal Emulator - you can use this buttons and interact with Hodoor. Same principe as with RFID Tags on hardware client terminal.",
+	"Last month - this panel is showing you last month info. Working hours in last month etc.",
+	
+	"Duration of whole session from incomming to outgoing swipe.",
+	"Clean work time. This value should be assigned to projects.",
+	"Time, that was not assigned to projects. Should be 0 after end of month.",
+	"Number of breaks during this session.",
+	"Sum of duration of all breaks during this session.",
+	"Session is open, if there is no outgoing swipe.",
+	"Session is modified, if some of it's swipes were corrected.",
+	
+	"write next...",
 ];
 
 
@@ -13,10 +23,24 @@ var numOfTooltips = 0;
 
 /*--------Append all tooltips to their IDs----------*/
 $(document).ready(function() {
-	$('.question').each(function(i) {
+	var TooltipsIds = [];
+	var MinTooltipNum, MaxTooltipNum;
+
+	$('.question').each(function(i) { //question is tooltiped by default
 		numOfTooltips++;
+		TooltipsIds[i] = $(this).attr('id').replace('tooltip', ''); //get only tooltip id number
 	});
+	MinTooltipNum = Math.min.apply(Math,TooltipsIds);
+	MaxTooltipNum = Math.max.apply(Math,TooltipsIds);
+
 	for(var n=0; n<=numOfTooltips; n++) {
-		$('#tooltip'+n).attr('title', tooltipData[n-1]);
+		$('#tooltip'+(n+MinTooltipNum)).attr('title', tooltipData[n+MinTooltipNum-1]);
+		console.log(TooltipsIds[n]);
+		console.log('#tooltip'+(n+MinTooltipNum));
+		console.log(tooltipData[n+MinTooltipNum-1]);		
 	}
+	console.log(numOfTooltips);
+	console.log(MinTooltipNum);
+	console.log(MaxTooltipNum);
+
 });
