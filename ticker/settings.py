78dfb533d717207d7d6ca85e3ff5b7aa1d502be2
patrik.predny <patrik.predny@eledus.cz>
2017-04-27 15:02:04 +0200

    added node package manager for dependecies install

--- ticker/settings.py
+++ ticker/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "node_modules"),
+)
 
 SPAGHETTI_SAUCE = {
   'apps':['auth','attendance',],
