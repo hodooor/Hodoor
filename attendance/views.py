95695efab24fd084f4ef039182133b2599844101
Tomáš Houfek <tomas.houfek@eledus.cz>
2017-08-04 12:59:19 +0200

    Another database optimalization

--- attendance/views.py
+++ attendance/views.py
@@ -410,20 +410,18 @@ def administrator(request, year=str(datetime.now().year), month="{0:02d}".format
 
     locale.setlocale(locale.LC_ALL, "en_US.utf8")
     projects = Project.objects.all()
-    sessions = Session.objects.select_related("user").filter(project__name=project)
+    separations =  ProjectSeparation.objects.filter(project__name = project).select_related("session").select_related("session__user").select_related("project")
     users = User.objects.all()
     all_users_month, all_users_overall = timedelta(0), timedelta(0)
 
     for user in users:
         duration, overall_duration = timedelta(0), timedelta(0)
-        for session in sessions:
-            separations =  ProjectSeparation.objects.filter(session = session).select_related("session").select_related("project")
-            for sep in separations:
-                if(sep.project.name == project and session.user.username == user.username):
-                    overall_duration += sep.time_spend
-                    date = (session.get_date())
-                    if('{:02d}'.format(date.month) == month and str(date.year) == year):
-                        duration += sep.time_spend
+        for sep in separations:
+            if(sep.project.name == project and sep.session.user.username == user.username):
+                overall_duration += sep.time_spend
+                date = (sep.session.get_date())
+                if('{:02d}'.format(date.month) == month and str(date.year) == year):
+                    duration += sep.time_spend
         projects_data.append({
                 "user": user,
                 "hours": duration,
